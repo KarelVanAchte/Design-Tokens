d58ce5fed5a43a4dd9e84344864fc3846f059f6c
kvachte <karel.vanachte@ugent.be>
2024-03-21 13:29:00 +0100

    Update parse script to remove core.neutrals for Figma Token Studio

--- parsers/transformDesignTokens.js
+++ parsers/transformDesignTokens.js
@@ -8,7 +8,8 @@ function transformDesignTokens(inputJson, modifyReferences = false) {
         if (typeof value === 'object' && value !== null) {
           value = transformObject(value); // Recursive call to transform nested objects
         } else if (typeof value === 'string' && modifyReferences) {
-          value = value.replace(/\{core\.colors\.(.*?)\}/g, '{colors.$1}'); // Modify references
+          //value = value.replace(/\{core\.colors\.(.*?)\}/g, '{colors.$1}'); // Modify references
+          value = value.replace(/\{core\.(colors|neutrals)\.(.*?)\}/g, '{$1.$2}');
         }
   
         transformed[newKey] = value;
