467798fc786e419ca468db8a1bc1861c74637217
kvachte <karel.vanachte@ugent.be>
2024-03-28 16:51:37 +0100

    Style dictionary scss build

--- build.js
+++ build.js
@@ -1,12 +1,23 @@
 const StyleDictionary = require('style-dictionary').extend({
     source: ['tokens/parsed/style-dictionary/tokens.json'],
     platforms: {
-      scss: {
+      css: {
         transformGroup: 'web',
         buildPath: 'build/',
         files: [{
           destination: 'variables.css',
           format: 'css/variables',
+          filter: (token) => {
+            return !token.name.startsWith('core-colors');
+          }
+        }]
+      },
+      scss: {
+        transformGroup: 'scss',
+        buildPath: 'build/',
+        files: [{
+          destination: '_variables.scss"',
+          format: 'scss/variables',
           filter: (token) => {
             return !token.name.startsWith('core-colors');
           }
